ae1d3e9c4fc4e3063f27d96396188f74412f9031
pr0gstar <ch.planken@gmail.com>
2020-06-12 14:15:28 +0200

    ...

--- scripts/check-cameras.py
+++ scripts/check-cameras.py
@@ -1,4 +1,3 @@
-import cv2
 import time
 import datetime
 
@@ -7,21 +6,5 @@ for i in range(0, 5):
     is_camera = cap.isOpened()
     if is_camera:
         print(f"Input {i} is a valid camera value for VIDEO_SOURCE")
-        ret, frame = cap.read() 
-        if ret == True:
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            text = 'Width: ' + str(cap.get(3)) + ' Height:' + str(cap.get(4))
-            datet = str(datetime.datetime.now())
-            frame = cv2.putText(frame, text, (10, 50), font, 1,
-                                (0, 255, 255), 2, cv2.LINE_AA)
-            frame = cv2.putText(frame, datet, (10, 100), font, 1,
-                                (0, 255, 255), 2, cv2.LINE_AA)
-            cv2.imshow('frame', frame)
-
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-            else:
-                break
-
         cap.release()
         time.sleep(3)
