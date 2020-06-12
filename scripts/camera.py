21729c9871fae8c77a2f98bf594e9da1ee1d558c
pr0gstar <ch.planken@gmail.com>
2020-06-12 13:55:19 +0200

    config file path

--- scripts/camera.py
+++ scripts/camera.py
@@ -13,7 +13,8 @@ from imutils import opencv2matplotlib
 
 from PIL import Image
 
-CONFIG_FILE_PATH = os.getenv("MQTT_CAMERA_CONFIG", "./config/config.yml")
+# CONFIG_FILE_PATH = os.getenv("MQTT_CAMERA_CONFIG", "./config/config.yml")
+CONFIG_FILE_PATH = "./config/config.yml"
 CONFIG = get_config(CONFIG_FILE_PATH)
 
 MQTT_BROKER = CONFIG["mqtt"]["broker"]
